14ee81e5bf09301b515f7df2fd814d05db5b54a9
daiweijie <690385484@qq.com>
2017-06-04 12:50:35 +0800

    安装第三方库

---
+++ pillow/thumb.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+def thumb():
+    im = Image.open('image1.png')
+    print(im.format,im.size,im.mode)
+    im.thumbnail((100,100))
+    im.save('thumb.png','PNG')
+
+
+thumb()
